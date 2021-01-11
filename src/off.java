d2c9cfcdda5d4393a7bbc2d8213e238b81e0aff6
amit-os <amitos@post.bgu.ac.il>
2021-01-11 12:40:57 +0200

    off added

---
+++ src/off.java
@@ -0,0 +1,9 @@
+public class off extends State{
+
+    public off(AirConditioner airCondition){
+        AirConditioner context = airCondition;
+    }
+    @Override
+    public void setCurrent_state(State st) {
+    }
+}
