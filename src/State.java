93cfc7f50a7a0cce1b19a87db631c5ed6419083c
אורי בן-ארצי <oribena@post.bgu.ac.il>
2021-01-11 12:05:45 +0200

    State

--- src/State.java
+++ src/State.java
@@ -1,3 +1,18 @@
 public abstract class State {
+    public void entry(){}
+
+    public void exit(){
+        System.out.print("Good Bye");
+    }
+
+    public void setC_temp(int C_temp) { }
+
+    public void setR_temp(int R_temp) { }
+
+    public void setCool(boolean cool) { }
+
+    public void ChangeState(boolean isCool) {
+
+    }
 
 }
