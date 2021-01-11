1eeb63625ea7525d53d4c4a3cb966d3e751cfda3
morzw <morzw@post.bgu.ac.il>
2021-01-11 13:20:55 +0200

    cooling + heating

--- src/Cooling.java
+++ src/Cooling.java
@@ -1,4 +1,11 @@
 public class Cooling extends State {
+
+    private Operation context;
+
+    public Cooling(Operation context) {
+        this.context = context;
+    }
+
     @Override
     public void entry() {
         System.out.println("OPERATION-COOLING");
--- src/Heating.java
+++ src/Heating.java
@@ -1,4 +1,11 @@
 public class Heating extends State {
+
+    private Operation context;
+
+    public Heating(Operation context) {
+        this.context = context;
+    }
+
     @Override
     public void entry() {
         System.out.println("OPERATION-HEATING");
